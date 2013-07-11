a27f6ab48a76bc238397aaac9fa337e08bc24a14
iusong <yunfeng.cai@gmail.com>
2013-07-11 08:02:28 +0000

    debug web.js 2nd

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
+var fs = (require 'fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var output = fs.readFileSync("./index.html");
 app.get('/', function(request, response) {
-  var output = fs.readFileSync("./index.html");
 //  var buf = new Buffer(output.length);
 //  buf.write(output,"utf-8");
 //  response.send(buf.toString('utf-8'));

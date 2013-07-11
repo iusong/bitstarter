0aaa2ef3fc7c5c6cb596fa7016a36670bc3e1a02
iusong <yunfeng.cai@gmail.com>
2013-07-11 08:56:13 +0000

    dddd

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var output = fs.readFileSync("./index.html");
+  var output = fs.readFileSync("index.html");
   var buf = new Buffer(output,"utf-8");
 //  var end = buf.write(output,"utf-8");
   response.send(buf.toString("utf-8",0,output.length));

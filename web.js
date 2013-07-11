39e3db497b5811eb993b33e944f3fbf6242a6980
iusong <yunfeng.cai@gmail.com>
2013-07-11 07:52:28 +0000

    add buffer to web.js

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var output = fs.readFileSync("./index.html");
+  var buf = new Buffer(output.length);
+  buf.write(output,"utf-8");
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
